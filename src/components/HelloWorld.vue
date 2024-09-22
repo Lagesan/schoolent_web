7defe0f31d873736cad687c9ed76ba706ea9e320
lagesan <observer.lagesan@outlook.com>
2024-09-22 19:42:19 +0800

    basic codes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,20 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+<div>{{ msg }}</div>
+<button :title="test" @click="showAlert">{{ test }}</button>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+<script>
+export default{
+  data(){
+    return{
+      msg:"Hello Schoolent!",
+      test:"测试按钮"
+    }
+  },
+  methods: {
+    showAlert() {
+      alert("Schoolent is coming!");
+    }
   }
 }
-</style>
+</script>
